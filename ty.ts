f2cd5427a237f11c6c6ccae161fa7a08a2400146
mehul6611 <rathodmehul99770@gmail.com>
2024-07-16 12:12:21 +0530

    Add files via upload

---
+++ ty.ts
@@ -0,0 +1,6 @@
+import type { LinksFunction } from "@remix-run/node";
+import stylesheet from "~/tailwind.css?url";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
+];
